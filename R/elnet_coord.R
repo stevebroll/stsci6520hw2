24a60dc3c9049d0c2bf3c97b50b0547521a4f69a
stevebroll <stevebroll@outlook.com>
2021-05-12 21:45:01 -0400

    update elastic net output objects

--- R/elnet_coord.R
+++ R/elnet_coord.R
@@ -2,9 +2,9 @@
 #'
 #' @param x Predictor matrix x, with n rows and p columns
 #' @param y Response vector y, with n elements
-#' @param lambda Lasso tuning parameter
 #' @param alpha Elastic net alpha parameter. elastic net simplifies to Ridge
 #'  regression under alpha = 0, and to Lasso regression under alpha = 1
+#' @param lambda Lasso tuning parameter
 #' @param tol Tolerance for convergence of betas. Algorithm will complete when
 #' the maximum change in beta values is smaller than the tol value
 #'
@@ -17,24 +17,27 @@
 #' covar = diag(1,20)
 #' covar[1,2] = covar[2,1] = covar[5,6] = covar[6,5] = .8
 #' x = MASS::mvrnorm(20, rep(0, 20), covar)
-#' y = x %*% beta + rnorm(20s, 0, 1)
+#' y = x %*% beta + rnorm(20, 0, 1)
 #'
-elnet_coord <- function(x, y, lambda, alpha, tol){
-  x = scale(x)
+elnet_coord <- function(x, y, alpha, lambda = .1, tol = .0001){
+  if(alpha < 0 || alpha > 1){
+    stop("alpha should be in [0,1]")
+  }
+  x = as.matrix(scale(x))
+  p = ncol(x)
   la = lambda*(1- alpha) + 1
-  betahat = rep(0,20)
-  betamat = matrix(0, 20, 1)
+  betahat = rep(0,p)
   maxdiff = 1
-  i = 2
   while(maxdiff > tol){
-    for(j in 1:ncol(x)){
+    old_betahat = betahat
+    for(j in 1:p){
       r = y - x[,-j] %*% betahat[-j]
       rx = 1/nrow(x) * t(r) %*% x[,j]
       betahat[j] = soft(rx, lambda, alpha)/la
     }
-    betamat = cbind(betamat, betahat)
-    maxdiff = max(abs(betamat[, i] - betamat[, i-1]))
-    i = i + 1
+    maxdiff = max(abs(betahat - old_betahat))
   }
-  return(betamat)
+  betahat = data.frame(Beta = betahat)
+  #cat(paste("Betas for alpha = ", alpha, " and lambda = ", lambda, ": \n"))
+  return(betahat)
 }
